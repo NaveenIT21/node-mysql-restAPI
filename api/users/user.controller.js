aa85705174c7e478a2cae5f84325a1ee2533a302
Naveen <naveenit21@gmail.com>
2019-12-02 00:11:37 +0530

    updated

--- api/users/user.controller.js
+++ api/users/user.controller.js
@@ -1,5 +1,6 @@
-const { create } = require("./user.service");
-const { genSaltSync, hashSync } = require('bcrypt');
+const { create, getUsers, getUserByUserId, updateUser, deleteUser, getUserByUserEmail } = require("./user.service");
+const { genSaltSync, hashSync, compareSync } = require('bcrypt');
+const { sign } = require('jsonwebtoken');
 module.exports = {
     createUser: (req, res) => {
         const body = req.body;
@@ -20,5 +21,108 @@ module.exports = {
                 data: results
             })
         })
+    },
+    getUserByUserId: (req, res) => {
+        const id = req.params.id;
+        getUserByUserId(id,(err, results) => {
+            if(err){
+                console.log(err);
+                return;
+            }
+            if(!results){
+                return res.json({
+                    success: 0,
+                    message: "Record not Found"
+                });
+            }
+            return res.json({
+                success: 1,
+                data: results
+            });
+        });
+    },
+    getUsers: (req, res) => {
+        getUsers((err, results) => {
+            if(err){
+                console.log(err);
+                return;
+            }
+            return res.json({
+                success: 1,
+                data: results
+            });
+        });
+    },
+    updateUser: (req, res) => {
+        const body = req.body;
+        console.log(body);
+        
+        const salt = genSaltSync(10);
+        body.password = hashSync(body.password, salt);
+        updateUser(body, (err, results) => {
+            if(err){
+                console.log(err);
+                return false;
+            }
+            if(!results){
+                return res.json({
+                    success: 0,
+                    message: "Failed to update user"
+                });
+            }
+            return res.status(200).json({
+                success: 1,
+                mesage: "updated successfully"
+            })
+        })
+    },
+    deleteUser: (req, res) => {
+        const data = req.body;
+        deleteUser(data, (err, results) => {
+            if(err){
+                console.log(err);
+                return;
+            }
+            if(!results){
+                res.json({
+                    success: 0,
+                    mesage: "Recourd not Found"
+                });
+            }
+            return res.json({
+                success: 1,
+                message: "user deleted successfully"
+            });
+        });
+    },
+    login: (req, res) => {
+        const body = req.body;
+        getUserByUserEmail(body.email, (err, results) => {
+            if(err){
+                console.log(err);
+                return;
+            }
+            if(!results){
+                return res.json({
+                    success: 0,
+                    message: "Invalid email or password"
+                });
+            }
+            const result = compareSync(body.password, results.password);
+            if(result){
+                results.password = undefined;
+                const jsontoken = sign({result: results}, 'enk1234',{ expiresIn: "1h"});
+                return res.json({
+                    success: 1,
+                    message: "login successfully",
+                    token: jsontoken
+                });
+            }else{
+                return res.json({
+                    success: 0,
+                    message: "Invalid email or password"
+                });
+            }
+        });
     }
 }
--- api/users/user.service.js
+++ api/users/user.service.js
@@ -32,5 +32,61 @@ module.exports = {
                 return callBack(null, results);
             }
         )
-    }
+    },
+    getUserByUserId: (id, callBack) => {
+        pool.query(
+            `select id, firstName, lastName, gender, email, number from registration where id = ?`,
+            [id],
+            (error, results, fields) => {
+                if(error){
+                    callBack(error);
+                }
+                return callBack(null, results[0]);
+            }
+        );
+    },
+    updateUser: (data, callBack) => {
+        pool.query(
+            `update registration set firstName = ?, lastName = ?, gender = ?, email = ?, number = ? where id = ?`,
+            [
+                data.first_name,
+                data.last_name,
+                data.gender,
+                data.email,
+                data.number,
+                data.id
+            ],
+            (error, results, fields) => {
+                if(error){
+                    callBack(error);
+                }
+                return callBack(null, results);
+            }
+        );
+    },
+    deleteUser: (data, callBack) => {
+        pool.query(
+            `delete from registration where id = ?`,
+            [data.id],
+            (error, results, fields) => {
+                if(error){
+                    callBack(error);
+                }
+                return callBack(null, results);
+            }
+        )
+    },
+    getUserByUserEmail: (email, callBack) => {
+        pool.query(
+            `select * from registration where email = ?`,
+            [email],
+            (error, results, fields) => {
+                if(error){
+                    callBack(error);
+                }
+                return callBack(null, results[0]);
+            }
+        );
+    },
+
 };
