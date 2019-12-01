c752115cd70efdf66f65145705118aa9f0c16155
Naveen <naveenit21@gmail.com>
2019-12-01 17:05:21 +0530

    track

---
+++ api/users/user.service.js
@@ -0,0 +1,36 @@
+const pool = require("../../config/database");
+
+module.exports = {
+    create: (data, callBack) => {
+        pool.query(
+            `insert into registration(firstName, lastName, gender, email, password, number) 
+            values(?,?,?,?,?,?)`,
+            [
+                data.first_name,
+                data.last_name,
+                data.gender,
+                data.email,
+                data.password,
+                data.number
+            ],
+            (error, results, fields) => {
+                if(error){
+                    callBack(error);
+                }
+                return callBack(null, results);
+            }
+        )
+    },
+    getUsers: callBack => {
+        pool.query(
+            `select * from registration`,
+            [],
+            (error, results, fields) => {
+                if(error){
+                    return callBack(error);
+                }
+                return callBack(null, results);
+            }
+        )
+    }
+};
